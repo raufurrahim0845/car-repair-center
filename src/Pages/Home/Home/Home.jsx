747f458aff9e7e71f2aaf073bc778447ec940445
raufurrahim0845 <mdabdurrahfurrahim@gmail.com>
2024-04-20 19:34:34 +0600

    about us section has completed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
+
 
 const Home = () => {
     return (
         <div className="mt-5">
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,12 @@
+
+const Services = () => {
+    return (
+        <div>
+            <div>
+                <h3 className="text-3xl text-orange-600">Our Services</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
